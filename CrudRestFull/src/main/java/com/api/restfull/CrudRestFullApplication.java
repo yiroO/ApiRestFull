9136433df516b4824a92d5385e2d1f3309a8374d
yiroo <yiroo@DESKTOP-L1LG4HB>
2020-08-01 14:22:19 -0500

    First Commit

---
+++ CrudRestFull/src/main/java/com/api/restfull/CrudRestFullApplication.java
@@ -0,0 +1,13 @@
+package com.api.restfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRestFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudRestFullApplication.class, args);
+	}
+
+}
---
+++ CrudRestFull/src/test/java/com/api/restfull/CrudRestFullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.restfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRestFullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
